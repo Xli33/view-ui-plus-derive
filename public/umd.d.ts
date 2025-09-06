e1414d76a0e772a940181c23a01ef78828f9799b
Xli33 <lshin1232@gmail.com>
2025-09-06 19:08:20 +0800

    feat: 优化umd.d.ts声明

--- public/umd.d.ts
+++ public/umd.d.ts
@@ -1,1 +1,18 @@
-declare module 'view-ui-plus-derive/umd/index' {}
+import type { App } from 'vue'
+
+declare global {
+  interface Window {
+    iviewDerive: {
+      install(
+        app: App,
+        opt?: {
+          i18n?: any
+          msg?: Record<string, any>
+          msgPrefix?: string
+        }
+      ): void
+    }
+  }
+}
+
+// declare module 'view-ui-plus-derive/umd/index' {}
